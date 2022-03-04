5fcbc4a1205f60cb3a094320daaeee6c63322926
sxxb <scottbelcher@live.com.au>
2022-03-04 13:48:34 +1030

    CV Styling

--- src/components/CV.js
+++ src/components/CV.js
@@ -2,115 +2,115 @@ import React from 'react';
 
 function Experience(props) {
   return (
-    <li className="item">
+    <section className="item">
       <div className="heading  flex row">
         <div className="title">
-          <h3>{props.jobTitle}</h3>
-          <h4>{props.employer}</h4>
+          <h4>{props.jobTitle}</h4>
+          <h5>{props.employer}</h5>
         </div>
-        <div className="duration  flex row centre">
-          <h5>{props.startDate}</h5>
-          <h6>to</h6>
-          <h5>{props.endDate}</h5>
+        <div id="duration" className="flex row centre">
+          <h6>{props.startDate}</h6>
+          <p>to</p>
+          <h6>{props.endDate}</h6>
         </div>
       </div>
       <div className="description">
         <p>{props.description}</p>
       </div>
-    </li>
+    </section>
   )
 }
 function Education(props) {
   return (
-    <li className="item">
+    <section className="item">
       <div className="heading flex row">
         <div className="title">
-          <h3>{props.degree}</h3>
-          <h4>{props.university}</h4>
+          <h4>{props.degree}</h4>
+          <h5>{props.university}</h5>
         </div>
-        <div className="duration  flex row centre">
-          <h5>{props.startDate}</h5>
-          <h6>to</h6>
-          <h5>{props.endDate}</h5>
+        <div id="duration" className="flex row centre">
+          <h6>{props.startDate}</h6>
+          <p>to</p>
+          <h6>{props.endDate}</h6>
         </div>
       </div>
       <div className="description">
         <p>{props.description}</p>
       </div>
-    </li>
+    </section>
   )
 }
 function Reference(props) {
   return (
-    <li className="item">
+    <section className="item">
       <div className="heading">
         <div className="title">
-          <h3>{props.name}</h3>
-          <h4>{props.employer}</h4>
+          <h4>{props.name}</h4>
+          <h5>{props.employer}</h5>
         </div>
       </div>
       <div className="description">
         <p><a href={"mailto:" + props.email} >{props.email}</a> + <a href={"tel:" + props.phone}>{props.phone}</a></p>
       </div>
-    </li>
+    </section>
   )
 }
 
 function Cv() {
   return (
-    <main className="cv">
-      <div className="flex row">
+    <main id="cv">
+      <div id="cvHead" className="flex row">
         <div>
           <h1>Scott Belcher</h1>
           <h2>C.V</h2>
         </div>
-        <div className="flex column contact-info">
+        <div id="contact-info" className="flex column">
           <a href="sms:0400123456">0400 123 456</a>
           <a href="mailto:email@domain.com">email@domain.com</a>
           <a href="/">Something Else</a>
         </div>
       </div>
-      <div className="flex column">
-        <div className="experience section">
-          <h2>Experience</h2>
-          <ul>
+      <div id="cvBody" className="flex column">
+        <div id="experience" className="section">
+          <h3>Experience</h3>
+          <div>
             <Experience jobTitle="Marketing and Design Officer" employer="Wavecom" startDate="2017" endDate="Present" description="tbc" />
             <Experience jobTitle="M" employer="W" startDate="2017" endDate="Present" description="tbc" />
-          </ul>
-        </div>
-        <div className="education section">
-          <h2>Education</h2>
-          <ul>
-            <Education degree="Bachelor of Economics" university="University of Adelaide" startDate="2018" endDate="2021" description="tbc" />
-          </ul>
-        </div>
-        <div className="skills section">
-          <h2>Skills</h2>
-          <div className="skills flex">
-          <ul className="break">
-            <li>Policy Analysis</li>
-            <li>Copy Writing</li>
-            <li>Team Organisation</li>
-            <li>Event Organisation</li>
-            <li>Technical Document Production</li>
-            <li>Web Design (HTML, CSS, Sass, Jekyll)</li>
-          </ul>
-          <ul className="break">
-            <li>Print and Packaging Design</li>
-            <li>Adobe Illustrator</li>
-            <li>Adobe Indesign</li>
-            <li>Adobe Premiere</li>
-            <li>Adobe Photoshop</li>
-            <li>Google Ads, Facebook Advertising</li>
-          </ul>
           </div>
         </div>
-        <div className="references section">
-          <h2>References</h2>
-          <ul>
+        <div id="education" className="section">
+          <h3>Education</h3>
+          <div>
+            <Education degree="Bachelor of Economics" university="University of Adelaide" startDate="2018" endDate="2021" description="tbc" />
+          </div>
+        </div>
+        <div id="skills" className="section">
+          <h3>Skills</h3>
+          <div className="flex row item">
+            <ul className="break">
+              <li>Policy Analysis</li>
+              <li>Copy Writing</li>
+              <li>Team Organisation</li>
+              <li>Event Organisation</li>
+              <li>Technical Document Production</li>
+              <li>Web Design (HTML, CSS, Sass, Jekyll)</li>
+            </ul>
+            <ul className="break">
+              <li>Print and Packaging Design</li>
+              <li>Adobe Illustrator</li>
+              <li>Adobe Indesign</li>
+              <li>Adobe Premiere</li>
+              <li>Adobe Photoshop</li>
+              <li>Google Ads, Facebook Advertising</li>
+            </ul>
+          </div>
+        </div>
+        <div id="references" className="section">
+          <h3>References</h3>
+          <div>
             <Reference name="Les Clifford" employer="Wavecom" email="les.clifford@wavecom.com.au" phone="xyz" />
             <Reference name="Jamie Gatt" employer="Lightime Illumination" email="X@Y" phone="xyz" />
-          </ul>
+          </div>
         </div>
       </div>
     </main>
