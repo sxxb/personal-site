059b67d98d7ff6e0c54cca089bb1d2b93747c7a8
sxxb <scottbelcher@live.com.au>
2022-03-07 15:58:15 +1030

    Updates

---
+++ precache-manifest.021386f00a71abb9c8cfcdd4b62f4c07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9804b6916175c4039f93",
+    "url": "/personal-site/static/css/main.747c84a4.chunk.css"
+  },
+  {
+    "revision": "9804b6916175c4039f93",
+    "url": "/personal-site/static/js/main.00a24038.chunk.js"
+  },
+  {
+    "revision": "d33cc63b615d83e32684",
+    "url": "/personal-site/static/js/runtime~main.039145d2.js"
+  },
+  {
+    "revision": "4fd983eb874ba830f378",
+    "url": "/personal-site/static/js/2.b23ae85a.chunk.js"
+  },
+  {
+    "revision": "e9c3635e220bf0d959d36b438c142aeb",
+    "url": "/personal-site/index.html"
+  }
+];
