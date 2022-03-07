b7bab9517f80b7496839f5e0b438e8d32b85a812
sxxb <scottbelcher@live.com.au>
2022-03-07 12:32:27 +1030

    Moved Figure Component to projects folder

--- src/components/projects/Tutorial-Videos.js
+++ src/components/projects/Tutorial-Videos.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
-import Figure from "../Figure"
+import Figure from "./Figure"
 import ProjectHeader from "./ProjectHeader"
 
 function Ptvideos() {
